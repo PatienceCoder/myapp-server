dcf30406f6b4a2665fb4d46debeb9c080a3b5071
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2024-06-19 18:19:54 +0530

    updated the origin value

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin : 'http://localhost:5173',
+    origin : 'https://myapp-server-gm8t.onrender.com',
     credentials:true
 }))
 app.get('/',(req,res) => {
