c4b43cbb174898ed4032807aa12e7eef500bd02f
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2024-06-19 20:15:48 +0530

    updated the value of origin with the URL given by vercel

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin : 'https://myappclient100.vercel.app/',
+    origin : 'https://myappclient100.vercel.app',
     credentials:true
 }))
 app.get('/',(req,res) => {
