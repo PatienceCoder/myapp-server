1e687dadeb1f8dea3fcfdfd2e985df9fbbbd6c40
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2024-06-19 18:32:34 +0530

    added * for origin property

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin : 'https://myapp-server-gm8t.onrender.com',
+    origin : '*',
     credentials:true
 }))
 app.get('/',(req,res) => {
